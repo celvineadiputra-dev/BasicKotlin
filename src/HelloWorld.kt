067937e6985cdd8035b11f0d7ed96bdeb98600b7
celvine <celvineadiputra2@gmail.com>
2025-09-04 16:08:25 +0700

    Update : [HelloWorld]

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,11 +1,15 @@
+// Fungsi utama yang akan dijalankan pertama kali
 fun main() {
-    // Print text hello world
-    println("Hello World")
+    // Menampilkan teks ke layar
+    println("Halo Dunia") // println akan mencetak teks dan pindah ke baris baru
 
     /*
-    * Ini adalah komentar jika
-    * lebih dari 1 baris
-    * */
-    print("Selamat Malam! ")
-    print("Nama Kamu")
+     Ini adalah komentar panjang.
+     Komentar seperti ini bisa digunakan untuk menjelaskan bagian kode.
+     Komputer akan mengabaikan komentar saat menjalankan program.
+    */
+
+    // Menampilkan teks tanpa pindah baris
+    print("Selamat malam! ")
+    print("Siapa namamu?")
 }
