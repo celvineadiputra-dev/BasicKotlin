5456b118de0018139d35f5ba25901d15b76967a7
celvine <celvineadiputra2@gmail.com>
2025-09-05 08:42:00 +0700

    Add : [Latihan & Contoh Variable, number, floating-point]

---
+++ src/Number.kt
@@ -0,0 +1,23 @@
+fun main() {
+    // Integer types
+    val age : Byte = 27
+    val weight : Short = 120
+    val distance : Int = 86000
+    val balance : Long = 3000000000L
+
+    println(age)
+    println(weight)
+    println(distance)
+    println(balance)
+
+    // Unsigned integer types
+    val uAge : UByte = 27u
+    val uWeight : UShort = 120u
+    val uDistance : UInt = 86000U
+    val uBalance : ULong = 3000000000uL
+
+    println(uAge)
+    println(uWeight)
+    println(uDistance)
+    println(uBalance)
+}
---
+++ src/Variable.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var playerScore = 100
+    println(playerScore)
+
+    playerScore = 103
+    println(playerScore)
+
+    val npm = "2428250030"
+    println(npm)
+}
