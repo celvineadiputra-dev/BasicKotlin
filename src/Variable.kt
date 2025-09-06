ec6f52e541c1e4a70509f4817555315d603001c7
celvine <celvineadiputra2@gmail.com>
2025-09-06 14:33:48 +0700

    Add : [Contoh const val]

--- src/Variable.kt
+++ src/Variable.kt
@@ -1,10 +1,21 @@
+const val APP_NAME = "Belajar Kotlin Dasar"
+const val APP_VERSION: String = "0.0.1"
+
 fun main() {
+    // Contoh penggunaan var
     var playerScore = 100
     println(playerScore)
 
-    playerScore = 103
+    playerScore = 103 // merubah isi dari variable var
     println(playerScore)
 
+    // Contoh penggunaan val
     val npm = "2428250030"
     println(npm)
+
+    //    npm = "123" // error
+    //    println(npm)
+
+    println(APP_NAME)
+    println(APP_VERSION)
 }
